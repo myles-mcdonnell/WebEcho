00af9bcc0370b552464743b2f10743a5ed29661d
mmcdonnell <Myles.McDonnell@eu.m3.com>
2015-08-19 16:03:29 +0100

    First cut

---
+++ src/EchoWeb4/Default.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="EchoWeb4._Default" %>
+
+
+<asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+    
+        <h1>ECHO</h1>
+    
+        <h2>Host: <asp:Label runat="server" id="HostLabel"></asp:Label></h2>
+        
+        <h2>Request - Misc</h2>
+        <ul>
+            <asp:Label runat="server" id="RequestMiscLabel"></asp:Label>
+        </ul>
+    
+        <h2>Request - Server Variable</h2>
+        <ul>
+            <asp:Label runat="server" id="ServerVariablesLabel"></asp:Label>
+        </ul>
+
+        <h2>Request - Headers</h2>
+        <ul>
+            <asp:Label runat="server" id="HeadersLabel"></asp:Label>
+        </ul>
+    
+        <h2>Request - Form Values</h2>
+        <ul>
+            <asp:Label runat="server" id="FormValuesLabel"></asp:Label>
+        </ul>
+
+</asp:Content>
---
+++ src/EchoWeb4/Default.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Web.UI;
+
+namespace EchoWeb4
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var labelHtml = new StringBuilder();
+
+            labelHtml.Append(string.Format("<li><b>{0}</b>:{1}</li>", "AcceptType", Request.AcceptTypes.Aggregate(string.Empty, (s, s1) => s + s1)));
+            labelHtml.Append(string.Format("<li><b>{0}</b>:{1}</li>", "ContentType", Request.ContentType));
+            labelHtml.Append(string.Format("<li><b>{0}</b>:{1}</li>", "ContentEncoding", Request.ContentEncoding));
+            labelHtml.Append(string.Format("<li><b>{0}</b>:{1}</li>", "HttpMethod", Request.HttpMethod));
+            labelHtml.Append(string.Format("<li><b>{0}</b>:{1}</li>", "UserHostName", Request.UserHostName));
+            labelHtml.Append(string.Format("<li><b>{0}</b>:{1}</li>", "UserHostAddress", Request.UserHostAddress));
+            labelHtml.Append(string.Format("<li><b>{0}</b>:{1}</li>", "Url", Request.Url));
+            labelHtml.Append(string.Format("<li><b>{0}</b>:{1}</li>", "IsSecureConnection", Request.IsSecureConnection));
+            
+            RequestMiscLabel.Text = labelHtml.ToString();
+
+            labelHtml.Clear();
+
+            foreach (var key in Request.Headers.AllKeys)
+                labelHtml.Append(string.Format("<li><b>{0}</b>:{1}</li>",
+                    key, Request.Headers[key]));
+
+            HeadersLabel.Text = labelHtml.ToString();
+
+            labelHtml.Clear();
+
+            foreach (var key in Request.Form.AllKeys)
+                labelHtml.Append(string.Format("<li><b>{0}</b>:{1}</li>",
+                    key, Request.Form[key]));
+
+            FormValuesLabel.Text = labelHtml.ToString();
+
+            labelHtml.Clear();
+
+            foreach (var key in Request.ServerVariables.AllKeys)
+                labelHtml.Append(string.Format("<li><b>{0}</b>:{1}</li>",
+                    key, Request.ServerVariables[key]));
+
+            ServerVariablesLabel.Text = labelHtml.ToString();
+
+            HostLabel.Text = Server.MachineName;
+        }
+    }
+}
